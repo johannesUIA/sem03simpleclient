22cdbf4f19a734b01498dfa7fdc06c7f50c549d1
johannesUIA <johannesma@uia.no>
2023-04-25 18:06:46 +0200

    prøvve

--- tcp_client.go
+++ tcp_client.go
@@ -9,24 +9,26 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "172.17.0.3:8040")
+	conn, err := net.Dial("tcp", "172.17.0.4:8040")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	log.Println("Din melding = ", os.Args[1])
 	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
-
 	log.Println("Kryptert melding: ", string(kryptertMelding))
-
 	_, err = conn.Write([]byte(string(kryptertMelding)))
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	buf := make([]byte, 1024)
 	n, err := conn.Read(buf)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, (len(mycrypt.ALF_SEM03) - 4))
-	log.Printf("reply from proxy: %s", string(dekryptertMelding))
+	response := string(buf[:n])
+	log.Printf("reply from proxy: %s", response)
 }
