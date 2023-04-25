67fe9c2eff9ad2355803da04e92b122223ce643d
johannesUIA <johannesma@uia.no>
2023-04-25 17:15:05 +0200

    super client

--- tcp_client.go
+++ tcp_client.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"net"
 	"log"
+	"net"
 	"os"
-        "github.com/johannesUIA/is105sem03/mycrypt"
 
+	"github.com/johannesUIA/is105sem03/mycrypt"
 )
 
 func main() {
@@ -13,24 +13,20 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-    
-	log.Println("os.Args[1] = ", os.Args[1])
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
 
- 	_, err = conn.Write([]byte(os.Args[1]))
-        if err != nil {
-         log.Fatal(err)  }
-        kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
-       log.Println("Kryptert melding: ", string(kryptertMelding))
-      _, err = conn.Write([]byte(string(kryptertMelding)))
-       if err != nil{
-		
-         log.Fatal(err)
+	log.Println("Kryptert melding: ", string(kryptertMelding))
+
+	_, err = conn.Write([]byte(string(kryptertMelding)))
+	if err != nil {
+		log.Fatal(err)
 	}
 	buf := make([]byte, 1024)
 	n, err := conn.Read(buf)
 	if err != nil {
 		log.Fatal(err)
-	} 
-	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
+	}
+
+	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, (len(mycrypt.ALF_SEM03) - 4))
+	log.Printf("reply from proxy: %s", string(dekryptertMelding))
 }
